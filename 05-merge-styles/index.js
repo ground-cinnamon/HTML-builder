cea69031491d553faa5a17e1f419b0a69bbbfd03
ground-cinnamon <morkovog@gmail.com>
2024-01-21 21:23:47 +0300

    feat: resolve 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const { readdir } = require('node:fs/promises');
+const { createReadStream, createWriteStream } = require('fs');
+const { join } = require('node:path');
+
+const folderPath = `${__dirname}/styles`;
+
+async function main() {
+  const files = await readdir(folderPath, { withFileTypes: true });
+  const writeStream = createWriteStream(
+    join(__dirname, 'project-dist', 'bundle.css'),
+  );
+
+  files.forEach(async (file) => {
+    if (!file.isFile() || !file.name.endsWith('.css')) return;
+
+    const stream = createReadStream(join(file.path, file.name));
+
+    stream.on('data', (data) => {
+      writeStream.write(data.toString() + '\n');
+    });
+  });
+}
+
+main();
